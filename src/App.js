8abe766e35ea9f46e12961f143ea2d71c6f03b7a
Jesse <themotleydev@gmail.com>
2019-12-06 11:13:35 +0100

    cleaned up process

--- src/App.js
+++ src/App.js
@@ -7,74 +7,80 @@ function App() {
   const [beerList, updateBeerList] = useState([]);
 
   useEffect(() => {
-    fetch("https://api-euwest.graphcms.com/v1/ck2delryp145j01et1z9mh04a/master", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query: `
-      {
-        beers {
-          name
-          recommendedGlass {
+
+    const fetchData = async () => {
+      const response = await await fetch("https://api-euwest.graphcms.com/v1/ck2delryp145j01et1z9mh04a/master", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query: `
+        {
+          beers {
             name
-          }
-          ratings {
-            dominantFlavors {
+            recommendedGlass {
               name
             }
-            mildFlavors {
+            ratings {
+              dominantFlavors {
+                name
+              }
+              mildFlavors {
+                name
+              }
+              approachFlavors {
+                name
+              }
+              approach
+              mouthFeel
+              finish
+              glass {
+                name
+              }
+            }
+            abv
+            brewery {
+              name
+              founded
+              website
+            }
+            style {
               name
             }
-            approachFlavors {
-              name
-            }
-            approach
-            mouthFeel
-            finish
-            glass {
-              name
-            }
-          }
-          abv
-          brewery {
-            name
-            founded
-            website
-          }
-          style {
-            name
-          }
-          images {
-            url
-            width
-          }
-          snaps {
-            event {
-              name
-            }
-            persons {
-              name
-              twitterHandle
-            }
-            image {
+            images {
               url
               width
             }
+            snaps {
+              event {
+                name
+              }
+              persons {
+                name
+                twitterHandle
+              }
+              image {
+                url
+                width
+              }
+            }
           }
         }
-      }
-      ` })
-    })
-      .then(res => res.json())
-      .then(res => {
-        updateBeerList(res.data.beers)
+        ` })
       })
+      const result = await response.json()
+      updateBeerList(result.data.beers)
+    };
+  
+    fetchData();
+  
   }, []);
 
   return (
     <div className="bg-gray-400 overflow-auto px-6 h-screen w-screen">
-      {beerList.map((beer, index) => (
+    <Sorry>
+      {beerList.length && beerList.map((beer, index) => (
         <BeerDetail key={index} {...beer} />
       ))}
+      </Sorry>
     </div>
   );
 }
